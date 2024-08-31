10547091473226accafcb10de44e95342cf8cd37
Seth Dowden <sethclarkdowden@gmail.com>
2024-08-30 23:09:52 -0700

    idk

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,9 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALPHAS=QWERTY
-
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_MOUSE_VI
+#define MIRYOKU_MEDIA_VI
 
 #define MIRYOKU_ALTERNATIVES_TAP_QWERTY \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
