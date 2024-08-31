b54b792b04e74805926f3c9b8611e820e3c4efbc
Seth Dowden <sethclarkdowden@gmail.com>
2024-08-31 00:30:50 -0700

    re

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS=QWERTY
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_MOUSE_VI
+#define MIRYOKU_MEDIA_VI
+
 #define MIRYOKU_ALTERNATIVES_TAP_QWERTY \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
